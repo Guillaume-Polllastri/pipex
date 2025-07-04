3937a3b61d858392f6f0b169372d669380b5df84
Guillaume Pollastri <gpollast@f6r5s14.clusters.42paris.fr>
2025-07-04 17:33:20 +0200

    ajout debut pipex, decouverte

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: gpollast <gpollast@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/04 10:54:05 by gpollast          #+#    #+#             */
-/*   Updated: 2025/07/04 11:22:41 by gpollast         ###   ########.fr       */
+/*   Updated: 2025/07/04 14:59:55 by gpollast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,39 @@
 #include "pipex.h"
 #include <sys/types.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-int main(void)
+int	main(int ac, char **av)
 {
-    pid_t   pid;
+	pid_t	pid;
+	int		status;
+	int		fd;
 
-    
-    return (0);
+	if (ac == 5)
+	{
+		fd = open(av[4], O_CREAT | O_WRONLY | O_TRUNC, 0644);
+		if (fd < 0)
+			return (0);
+		write(fd, "rondelle", 8);
+		close (fd);
+		pid = fork();
+		if (pid > 0)
+		{
+			wait(&status);
+			printf("PID Parent: %d\n", pid);
+			if (status > 0)
+				printf("L'enfant dit que les mots sont differents");
+			else
+				printf("L'enfant dit que les mots sont identiques");
+		}
+		else
+		{
+			printf("PID Enfant: %d\n", pid);
+			return (ft_strncmp(av[1], av[2], ft_strlen(av[1])));
+		}
+	}
+	return (0);
 }
