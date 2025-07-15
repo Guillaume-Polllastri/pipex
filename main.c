ca19cfe72afea410272bf703e9f22b2ce103d6a0
Guillaume Pollastri <gpollast@f6r3s8.clusters.42paris.fr>
2025-07-15 18:53:37 +0200

    feat debut pipe

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: gpollast <gpollast@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/04 10:54:05 by gpollast          #+#    #+#             */
-/*   Updated: 2025/07/04 14:59:55 by gpollast         ###   ########.fr       */
+/*   Updated: 2025/07/15 17:50:01 by gpollast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,30 +21,43 @@
 
 int	main(int ac, char **av)
 {
-	pid_t	pid;
+	pid_t	pid1;
+	pid_t	pid2;
 	int		status;
 	int		fd;
+	int		pipefd[2];
+	char	*args[3];
+	char	*args2[3];
 
+	args[0] = "ls";
+	args[1] = "-la";
+	args[2] = NULL;
+	args2[0] = "wc";
+	args2[1] = "-l";
+	args2[2] = NULL;
 	if (ac == 5)
 	{
+		if (pipe(pipefd) == -1)
+		{
+			perror("pipe");
+			return (1);
+		}
 		fd = open(av[4], O_CREAT | O_WRONLY | O_TRUNC, 0644);
 		if (fd < 0)
 			return (0);
 		write(fd, "rondelle", 8);
 		close (fd);
-		pid = fork();
-		if (pid > 0)
+		pid1 = fork();
+		if (pid1 > 0)
 		{
-			wait(&status);
-			printf("PID Parent: %d\n", pid);
-			if (status > 0)
-				printf("L'enfant dit que les mots sont differents");
-			else
-				printf("L'enfant dit que les mots sont identiques");
+			printf("PID Enfant 1: %d\n", pid1);
+			execve("/bin/ls", args, NULL);
 		}
-		else
+		pid2 = fork();
+		if (pid2 > 0)
 		{
-			printf("PID Enfant: %d\n", pid);
+			printf("PID Enfant 2: %d\n", pid2);
+			execve("/bin/wc", args2, NULL);
 			return (ft_strncmp(av[1], av[2], ft_strlen(av[1])));
 		}
 	}
