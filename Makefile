602ff849aa4115f3098f6bfc0aec2801b2e874ec
Guillaume Pollastri <gpollast@f6r2s14.clusters.42paris.fr>
2025-07-17 18:35:38 +0200

    feat write on file2

--- Makefile
+++ Makefile
@@ -6,14 +6,15 @@
 #    By: gpollast <gpollast@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/07/04 10:53:47 by gpollast          #+#    #+#              #
-#    Updated: 2025/07/16 14:39:23 by gpollast         ###   ########.fr        #
+#    Updated: 2025/07/17 16:09:16 by gpollast         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME = pipex
 
 SRC = 	src/main.c \
-		src/parse.c
+		src/parse.c \
+		src/get_next_line.c
 
 OBJ = $(SRC:.c=.o)
 
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: gpollast <gpollast@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/04 10:54:05 by gpollast          #+#    #+#             */
-/*   Updated: 2025/07/16 15:54:45 by gpollast         ###   ########.fr       */
+/*   Updated: 2025/07/17 17:13:01 by gpollast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,69 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include "get_next_line.h"
+
+static int	child1(pid_t pid, int *pipefd, char **av)
+{
+	char	*path;
+	char	**cmd;
+
+	if (pid == 0)
+	{
+		close(pipefd[0]);
+		dup2(pipefd[1], STDOUT_FILENO);
+		close(pipefd[1]);
+		path = cmd_path(av[2]);
+		cmd = ft_split(av[2], " \t");
+		execve(path, cmd, NULL);
+		perror("Error\nEnfant 1 Invalid command ");
+		return (0);
+	}
+	return (0);
+}
+
+static int	child2(pid_t pid, int *pipefd, char **av)
+{
+	char	*path;
+	char	**cmd;
+
+	if (pid == 0)
+	{
+		close(pipefd[1]);
+		dup2(pipefd[0], STDIN_FILENO);
+		close(pipefd[0]);
+		path = cmd_path(av[3]);
+		cmd = ft_split(av[3], " \t");
+		execve(path, cmd, NULL);
+		perror("Error\nEnfant 2 Invalid command ");
+		return (0);
+	}
+	return (0);
+}
+
+static int	parent(pid_t pid, int status, int *pipefd)
+{
+	int		fd;
+	char	*line;
+
+	line = get_next_line(pipefd[0]);
+	fd = open("file2.txt", O_CREAT | O_WRONLY | O_TRUNC, 0644);
+	if (fd == -1)
+		return (perror("Error\n"), 1);
+	while (line)
+	{
+		write(fd, line, ft_strlen(line));
+		free(line);
+		line = get_next_line(pipefd[0]);
+	}
+	close(pipefd[0]);
+	close(pipefd[1]);
+	close(fd);
+	waitpid(pid, &status, 0);
+	printf("Enfant 2 terminé\n");
+	printf("Les deux Enfants ont été crées\n");
+	return (1);
+}
 
 int	main(int ac, char **av)
 {
@@ -25,8 +88,6 @@ int	main(int ac, char **av)
 	pid_t	pid2;
 	int		status;
 	int		pipefd[2];
-	char	*path;
-	char	**cmd;
 
 	if (ac == 5)
 	{
@@ -36,42 +97,14 @@ int	main(int ac, char **av)
 			return (1);
 		}
 		pid1 = fork();
-		if (pid1 == 0)
-		{
-			printf("PID Enfant 1: %d\n", getpid());
-			close(pipefd[0]);
-			dup2(pipefd[1], STDOUT_FILENO);
-			close(pipefd[1]);
-			path = cmd_path(av[2]);
-			cmd = ft_split(av[2], " \t");
-			execve(path, cmd, NULL);
-			perror("Error\nEnfant 1 Invalid command ");
-			return (1);
-		}
+		child1(pid1, pipefd, av);
 		waitpid(pid1, &status, 0);
 		printf("Enfant 1 terminé\n");
 		pid2 = fork();
-		if (pid2 == 0)
-		{
-			printf("PID Enfant 2: %d\n", getpid());
-			close(pipefd[1]);
-			dup2(pipefd[0], STDIN_FILENO);
-			close(pipefd[0]);
-			path = cmd_path(av[3]);
-			cmd = ft_split(av[3], " \t");
-			execve(path, cmd, NULL);
-			perror("Error\nEnfant 2 Invalid command ");
-			return (1);
-		}
-		close(pipefd[0]);
-		close(pipefd[1]);
-		waitpid(pid2, &status, 0);
-		printf("Enfant 2 terminé\n");
-		printf("Les deux Enfants ont été crées\n");
+		child2(pid2, pipefd, av);
+		parent(pid2, status, pipefd);
 		return (0);
 	}
 	printf("Error\nThe program needs 4 arguments\n");
-	// free(path);
-	// free_string_array(cmd);
 	return (0);
 }
