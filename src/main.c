b688cbd98e1bf7956ee99aa0241cc7261d4cd3ad
Guillaume <guillox01@gmail.com>
2025-07-18 11:48:48 +0200

    fix exit correctly for child1 and child2, feat file1 and file2

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: gpollast <gpollast@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/04 10:54:05 by gpollast          #+#    #+#             */
-/*   Updated: 2025/07/17 17:13:01 by gpollast         ###   ########.fr       */
+/*   Updated: 2025/07/18 11:45:53 by gpollast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,66 +20,52 @@
 #include <fcntl.h>
 #include "get_next_line.h"
 
-static int	child1(pid_t pid, int *pipefd, char **av)
+static void	child1(pid_t pid, int *pipefd, char **av)
 {
 	char	*path;
 	char	**cmd;
+	int		fd;
 
 	if (pid == 0)
 	{
 		close(pipefd[0]);
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
+			exit(1);
+		dup2(fd, STDIN_FILENO);
+		close(fd);
 		dup2(pipefd[1], STDOUT_FILENO);
 		close(pipefd[1]);
 		path = cmd_path(av[2]);
 		cmd = ft_split(av[2], " \t");
 		execve(path, cmd, NULL);
 		perror("Error\nEnfant 1 Invalid command ");
-		return (0);
+		exit(EXIT_FAILURE);
 	}
-	return (0);
 }
 
-static int	child2(pid_t pid, int *pipefd, char **av)
+static void	child2(pid_t pid, int *pipefd, char **av)
 {
 	char	*path;
 	char	**cmd;
+	int		fd;
 
 	if (pid == 0)
 	{
 		close(pipefd[1]);
 		dup2(pipefd[0], STDIN_FILENO);
 		close(pipefd[0]);
+		fd = open(av[4], O_CREAT | O_WRONLY | O_TRUNC, 0644);
+		if (fd == -1)
+			exit(1);
+		dup2(fd, STDOUT_FILENO);
+		close(fd);
 		path = cmd_path(av[3]);
 		cmd = ft_split(av[3], " \t");
 		execve(path, cmd, NULL);
 		perror("Error\nEnfant 2 Invalid command ");
-		return (0);
+		exit(EXIT_FAILURE);
 	}
-	return (0);
-}
-
-static int	parent(pid_t pid, int status, int *pipefd)
-{
-	int		fd;
-	char	*line;
-
-	line = get_next_line(pipefd[0]);
-	fd = open("file2.txt", O_CREAT | O_WRONLY | O_TRUNC, 0644);
-	if (fd == -1)
-		return (perror("Error\n"), 1);
-	while (line)
-	{
-		write(fd, line, ft_strlen(line));
-		free(line);
-		line = get_next_line(pipefd[0]);
-	}
-	close(pipefd[0]);
-	close(pipefd[1]);
-	close(fd);
-	waitpid(pid, &status, 0);
-	printf("Enfant 2 terminé\n");
-	printf("Les deux Enfants ont été crées\n");
-	return (1);
 }
 
 int	main(int ac, char **av)
@@ -97,12 +83,26 @@ int	main(int ac, char **av)
 			return (1);
 		}
 		pid1 = fork();
+		if (pid1 == -1)
+		{
+			perror("fork");
+			return (1);
+		}
 		child1(pid1, pipefd, av);
-		waitpid(pid1, &status, 0);
-		printf("Enfant 1 terminé\n");
 		pid2 = fork();
+		if (pid2 == -1)
+		{
+			perror("fork");
+			return (1);
+		}
 		child2(pid2, pipefd, av);
-		parent(pid2, status, pipefd);
+		close(pipefd[1]);
+		close(pipefd[0]);
+		waitpid(pid1, &status, 0);
+		printf("Enfant 1 terminé avec status : %d\n", status);
+		waitpid(pid2, &status, 0);
+		printf("Enfant 2 terminé avec status : %d\n", status);
+		printf("Les deux Enfants ont été crées\n");
 		return (0);
 	}
 	printf("Error\nThe program needs 4 arguments\n");
